7b1b13cdf37b2b419550418895599fa7dfbdedef
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2022-09-10 22:35:14 +0100

    ui slice created

---
+++ src/store/ui-slice.ts
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+interface UIState {
+  isCartVisible: boolean
+}
+
+const initialState: UIState = {
+  isCartVisible: false
+}
+
+const uiSlice = createSlice({
+  name: 'ui',
+  initialState,
+  reducers: {
+    toggleCart(state) {
+      state.isCartVisible = !state.isCartVisible
+    }
+  }
+})
+
+export const uiActions = uiSlice.actions
+
+export default uiSlice
