c7cd3514121c7003926456630a335ae7aca35ff2
Arif Ebrahim <arif.ebrahim@sainsburys.co.uk>
2022-09-12 06:04:02 +0100

    Feature: basket saved to firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { useAppSelector } from './hooks';
+import { useAppSelector } from './hooks'
+import { useEffect } from 'react'
 
 import Layout from './components/Layout/'
 import Cart from './components/Cart/Cart'
-import Products from './components/Shop/Products';
+import Products from './components/Shop/Products'
 
 function App() {
   const isCartVisible = useAppSelector(state => state.ui.isCartVisible)
+  const cart = useAppSelector(state => state.cart)
+  const URL: string = process.env.REACT_APP_FIREBASE_API ?? ''
+
+  useEffect(() => {
+    fetch(URL, {
+      method: 'PUT',
+      body: JSON.stringify(cart)
+    })
+  }, [cart, URL])
 
   return (
     <Layout>
